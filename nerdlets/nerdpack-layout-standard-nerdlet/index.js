06d835f64928abd9420bdf1f8c5f496a3bae49b9
Daniel Golden <dgolden@newrelic.com>
2019-12-30 09:49:02 -0500

    fix: properly name grid sections
    newrelic/open-source-office#22

--- nerdlets/nerdpack-layout-standard-nerdlet/index.js
+++ nerdlets/nerdpack-layout-standard-nerdlet/index.js
@@ -86,7 +86,7 @@ export default class NerdpackLayoutTBone extends React.Component {
                 gapType={Stack.GAP_TYPE.NONE}
               >
                 <StackItem>
-                  <h4 className="empty-state-header">Column 1</h4>
+                  <h4 className="empty-state-header">Section 1</h4>
                 </StackItem>
                 <StackItem>
                   <p className="empty-state-description">
@@ -114,7 +114,7 @@ export default class NerdpackLayoutTBone extends React.Component {
                 gapType={Stack.GAP_TYPE.NONE}
               >
                 <StackItem>
-                  <h4 className="empty-state-header">Column 2</h4>
+                  <h4 className="empty-state-header">Section 2</h4>
                 </StackItem>
               </Stack>
             </main>
@@ -131,7 +131,7 @@ export default class NerdpackLayoutTBone extends React.Component {
                 gapType={Stack.GAP_TYPE.NONE}
               >
                 <StackItem>
-                  <h4 className="empty-state-header">Column 3</h4>
+                  <h4 className="empty-state-header">Section 3</h4>
                 </StackItem>
               </Stack>
             </main>
@@ -148,7 +148,7 @@ export default class NerdpackLayoutTBone extends React.Component {
                 gapType={Stack.GAP_TYPE.NONE}
               >
                 <StackItem>
-                  <h4 className="empty-state-header">Column 4</h4>
+                  <h4 className="empty-state-header">Section 4</h4>
                 </StackItem>
               </Stack>
             </main>
